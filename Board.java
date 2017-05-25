4235dbfe1221f1c386749011ad8581b9c148d74c
gavmack <gavin_mackay@asl.org>
2017-05-25 08:27:58 +0100

    xd

--- Board.java
+++ Board.java
@@ -1,1 +1,18 @@
-
+public class Board {
+	private Tile[][] board;
+	
+	public Board() {
+		board = new Tile[8][8];
+		
+		for (int r = 0; r < board.length;r++) {
+			for (int c = 0; c < board[0].length;c++) {
+				board[r][c] = new Tile();
+			}
+		}
+		
+		board[3][3] = new Tile(true, true);
+		board[3][4] = new Tile(true, false);
+		board[4][3] = new Tile(true, false);
+		board[4][4] = new Tile(true, true);
+	}
+}
