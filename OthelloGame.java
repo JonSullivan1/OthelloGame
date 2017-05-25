41472c879f8ff8e723350bac02f5ae93b6782ee2
jonsullivan <jon_sullivan@asl.org>
2017-05-25 08:48:48 +0100

    asdf

--- OthelloGame.java
+++ OthelloGame.java
@@ -9,13 +9,7 @@ public class OthelloGame{
 		
 	}
 	
-	public boolean canPlace(int r, int c){
-		return !(board.getTile(r, c).isTaken())
-	}
 	
-	public void updateBoard(int r1, int r2, int c1, int c2) {
-		
-	}
 	
 	
 }
