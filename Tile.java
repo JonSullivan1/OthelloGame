aeedcd7ddb23cb1d69038c30bf29e20250cf97af
JonSullivan1 <jon_sullivan@asl.org>
2017-05-29 13:30:46 +0100

    Update Tile.java

--- Tile.java
+++ Tile.java
@@ -2,16 +2,41 @@ public class Tile{
 	private boolean used;
 	private boolean isWhite;
 	
-	Tile(){
+	public Tile(){
 		used = false;
 		isWhite = true;
+	
 	}
 	
-	Tile(boolean used, boolean isWhite){
+	public Tile(boolean used, boolean isWhite){
 		this.used = used;
 		this.isWhite = isWhite;
 	}
 	
+	public boolean isTaken(){
+		return used;
+	}
+	public boolean white(){
+		return isWhite;
+	}
+	
+	public void changeColor(){
+		isWhite = !isWhite;
+	}
+	public boolean equals (Tile other) {
+		return (other.isTaken()&&this.isTaken())&&(other.white()&&this.isWhite);
+	}
+	public String toString(){
+		if(!used){
+			return " E ";
+		}
+		else if(isWhite){
+			return " W ";
+		}
+		else{
+			return " B ";
+		}
+	}
 	
 	
 }
