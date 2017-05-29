5c657f675f574082597e562e93314a4d00b6842f
JonSullivan1 <jon_sullivan@asl.org>
2017-05-29 11:37:35 +0100

    Update Board.java

--- Board.java
+++ Board.java
@@ -1,5 +1,9 @@
 public class Board {
 	private Tile[][] board;
+
+	private int length;
+	private int width;
+
 	
 	/** move offset for row */
 	private static final int[] sOFFSET_MOVE_ROW = {-1, -1, -1,  0,  0,  1,  1,  1};
@@ -9,13 +13,14 @@ public class Board {
 	
 	public Board() {
 		board = new Tile[8][8];
-		
+		length = 8;
+		width  =8;
 		for (int r = 0; r < board.length;r++) {
 			for (int c = 0; c < board[0].length;c++) {
 				board[r][c] = new Tile();
 			}
 		}
-		
+		//
 		board[3][3] = new Tile(true, true);
 		board[3][4] = new Tile(true, false);
 		board[4][3] = new Tile(true, false);
@@ -25,10 +30,61 @@ public class Board {
 	public Tile getTile (int r, int c) {
 		return board[r][c];
 	}
+	public int getLength(){
+		return length;
+	}
+	public int getWidth(){
+		return width;
+	}
 	
 	public void setTile (Tile t, int r, int c) {
 		board[r][c] = t;
 	}
+	
+
+	
+	public boolean checkMove(int r, int c, boolean isWhite){
+		boolean possible = true;
+		boolean mid = false;
+		for(int i = c;i<board.length;i++){
+			if((isWhite != board[r][i].white()) && board[r][i].isTaken()){
+				mid = true;
+			}
+			else if(mid && board[r][i].isTaken() && isWhite == board[r][i].white()){
+				return true;
+			}
+		}
+		mid = false;
+		for(int i = c;i>=0;i--){
+			if((isWhite != board[r][i].white()) && board[r][i].isTaken()){
+				mid = true;
+			}
+			else if(mid && board[r][i].isTaken() && isWhite == board[r][i].white()){
+				return true;
+			}
+		}
+		mid = false;
+		for(int i = r;i<board[0].length;i++){
+			if((isWhite != board[i][c].white()) && board[i][c].isTaken()){
+				mid = true;
+			}
+			else if(mid && board[i][c].isTaken() && isWhite == board[i][c].white()){
+				return true;
+			}
+		}
+		mid = false;
+		for(int i = r;i>board[0].length;i--){
+			if((isWhite != board[i][c].white()) && board[i][c].isTaken()){
+				mid = true;
+			}
+			else if(mid && board[i][c].isTaken() && isWhite == board[i][c].white()){
+				return true;
+			}
+		}
+		
+		return false;
+		
+		}
 	
 	public void updateBoard(int r, int c, boolean isWhite) {
 		int rTemp = r;
@@ -76,4 +132,5 @@ public class Board {
 			}
 		}
 	}
+
 }
