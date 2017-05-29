3eee2671dc05fe33a5d3058e0cbdbe1463164154
JonSullivan1 <jon_sullivan@asl.org>
2017-05-29 17:13:29 +0100

    Update OthelloGame.java

--- OthelloGame.java
+++ OthelloGame.java
@@ -1,17 +1,25 @@
 public class OthelloGame{
-	
+
 	private static boolean isWhite = false;
 	private Board board;
+	private static boolean game;
 	public static void main(String[] args){
 		isWhite = false;
-		
+		game = true;
 		Board board = new Board();
-		//it work
-		System.out.println(board.checkMove(3, 5, isWhite));
-		//board.updateBoard(4,5);
-			
-		
-	}
 
+		System.out.println(board.checkMove(5, 4, isWhite));
+		board.updateBoard(3, 2, isWhite);
+		//board.updateBoard(4,5);
+		//board.checkMove(5, 4, isWhite);
+
+		for(int i =0;i<board.getLength();i++){
+			for(int z = 0;z<board.getWidth();z++){
+				System.out.print(board.getTile(i,z));
+			}
+			System.out.println();
+		}	
+	}
 }
+
 
