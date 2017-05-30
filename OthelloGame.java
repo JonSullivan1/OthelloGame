a20d72e09f72dc39a3e55dfff1fcd05d2b9dbd6c
Gavin M <gavin_mackay@asl.org>
2017-05-30 14:10:46 +0100

    update to text aspect
    logic is correct, needs to be update to graphics

--- OthelloGame.java
+++ OthelloGame.java
@@ -1,25 +1,71 @@
+import java.util.Scanner;
+
 public class OthelloGame{
 
 	private static boolean isWhite = false;
 	private Board board;
 	private static boolean game;
 	public static void main(String[] args){
-		isWhite = false;
+		isWhite = true;
 		game = true;
 		Board board = new Board();
+		boolean player1 = true;
+		int playerInX = 0;
+		int playerInY = 0;
+		boolean inValid = false;
+		Scanner in = new Scanner(System.in);
 
-		System.out.println(board.checkMove(5, 4, isWhite));
-		board.updateBoard(3, 2, isWhite);
-		//board.updateBoard(4,5);
-		//board.checkMove(5, 4, isWhite);
-
-		for(int i =0;i<board.getLength();i++){
-			for(int z = 0;z<board.getWidth();z++){
-				System.out.print(board.getTile(i,z));
+		while(game) {
+			if (isWhite) {
+				while (!inValid) {
+					System.out.println("Player 1, enter X coord from 0-7: ");
+					playerInX = in.nextInt();
+					System.out.println("Player 1, enter Y coord from 0-7: ");
+					playerInY = in.nextInt();
+					if (board.isValidMove(playerInY, playerInX, isWhite)) {
+						inValid = true;
+					}
+					else {
+						System.out.println("NOT VALID. RETRY.");
+					}
+				}
+				inValid = false;
+				board.effectMove(playerInY, playerInX, isWhite);
+				for(int i =0;i<board.getLength();i++){
+					for(int z = 0;z<board.getWidth();z++){
+						System.out.print(board.getTile(i,z));
+					}
+					System.out.println();
+				}
 			}
-			System.out.println();
-		}	
+			else {
+				while (!inValid) {
+					System.out.println("Player 2, enter X coord from 0-7: ");
+					playerInX = in.nextInt();
+					System.out.println("Player 2, enter Y coord from 0-7: ");
+					playerInY = in.nextInt();
+					if (board.isValidMove(playerInY, playerInX, isWhite)) {
+						inValid = true;
+					}
+					else {
+						System.out.println("NOT VALID. RETRY.");
+					}
+				}
+				inValid = false;
+				board.effectMove(playerInY, playerInX, isWhite);
+				for(int i =0;i<board.getLength();i++){
+					for(int z = 0;z<board.getWidth();z++){
+						System.out.print(board.getTile(i,z));
+					}
+					System.out.println();
+				}
+			}
+			isWhite = !isWhite;
+		}
 	}
 }
+
+
+
 
 
