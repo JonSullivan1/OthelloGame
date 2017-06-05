83db3be12de0bbd1d912b1308f4472806e0058e5
Gavin M <gavin_mackay@asl.org>
2017-06-05 10:39:07 +0100

    OthelloGame w/ Comments
    commented and javadoc'd

--- OthelloGame.java
+++ OthelloGame.java
@@ -12,12 +12,14 @@ public class OthelloGame{
 		while(game){
 			draw(board, isWhite);
 			isWhite = handleMouseClick(isWhite,board);
+			//Check if there are available moves for current player, switch if there 
+			//are no available moves for current, but available moves for opposite.
 			if(board.checkIfOver(isWhite) && !(board.checkIfOver(!isWhite))){
 				StdDraw.text(3.5, 4, "NO VALID MOVES, SWAPPING PLAYER");
 				StdDraw.pause(500);
 				isWhite = !isWhite;
 			}
-
+			//Check for game over of current player.
 			else if(board.checkIfOver(isWhite)){
 				System.out.println("le game over");
 				System.out.print("White Score: " + board.getWhite());
@@ -27,13 +29,19 @@ public class OthelloGame{
 			}
 		}
 	}
-
+	/**
+	 * Method that draws the graphical aspect of the game.
+	 * 
+	 * @param board
+	 * @param isWhite
+	 */
 	public static void draw(Board board, boolean isWhite) {
 		// TODO You have to write this
 		StdDraw.clear();
 		StdDraw.setScale(-1,8);
 		for(int i =0;i<board.getLength();i++) {
 			for(int c =0;c<board.getWidth();c++) {
+				//Draw board and placed tiles.
 				StdDraw.setPenColor(StdDraw.GREEN);
 				StdDraw.filledSquare(i, c, .5);
 				if(board.getTile(c,i).isWhite() && board.getTile(c, i).isTaken()) {
@@ -51,6 +59,7 @@ public class OthelloGame{
 
 			}
 		}
+		//Draw grid.
 		StdDraw.setPenColor(StdDraw.BLACK);
 		for (int i = 0; i < 9; i++) {
 			StdDraw.line(-0.5 + i, -0.5, -0.5 + i, 7.5);
@@ -58,6 +67,7 @@ public class OthelloGame{
 		for (int i = 0; i < 9; i++) {
 			StdDraw.line(-0.5, -0.5 + i, 7.5, -0.5 + i);
 		}
+		//Draw current player and score.
 		if(isWhite){
 			StdDraw.text(3.5, 7.7, "Current player: WHITE");
 		}
@@ -71,7 +81,13 @@ public class OthelloGame{
 	}
 
 
-
+	/**
+	 * Receives input from the mouse and effects the move on the board.
+	 * 
+	 * @param isWhite
+	 * @param board
+	 * @return
+	 */
 	public static boolean handleMouseClick(boolean isWhite, Board board) {
 		while (!StdDraw.mousePressed()) {
 			// Wait for mouse press
